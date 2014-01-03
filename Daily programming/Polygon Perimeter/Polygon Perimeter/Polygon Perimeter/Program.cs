3aad30906250e628534fa037f888960f71ecff62
enclame <enclame@gmail.com>
2014-01-03 15:29:22 -0500

    Created daily program
    daily program polygon perimeter completed

---
+++ Daily programming/Polygon Perimeter/Polygon Perimeter/Polygon Perimeter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygon_Perimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of sides and cirucmradius");
+
+            string input = Console.ReadLine();
+            string[] splitInput = input.Split(' ');
+            
+            int sides = Convert.ToInt32(splitInput[0]);
+            float circumradius = Convert.ToSingle(splitInput[1]);
+
+            float perimeter = findPerimeter(sides, circumradius);
+
+            Console.WriteLine("{0:N3}", perimeter);
+
+        }
+
+        static float findPerimeter(int n, float r)
+        {
+            int sides = n;
+            Console.WriteLine("sides: " + sides);
+            float radius = r;
+            Console.WriteLine("radius: " + radius);
+
+            float side = (float)(2 * r * Math.Sin(Math.PI / n));
+            Console.WriteLine("side length: " + side);
+            float perimeter = side * sides;
+            Console.WriteLine("perimeter: " + perimeter);
+            return perimeter;
+        }
+
+    }
+}
