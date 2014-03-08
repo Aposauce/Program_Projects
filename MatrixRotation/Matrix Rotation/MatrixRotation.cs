15e6367f162c479aff29411665b583fca74e4e11
Aposauce <enclame@gmail.com>
2014-03-08 03:11:45 -0500

    add 2 new problems for real

---
+++ MatrixRotation/Matrix Rotation/MatrixRotation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_Rotation
+{
+    public class MatrixRotation
+    {
+        static void Main(string[] args)
+        {
+            int[,] m = {{ 1  , 2  , 3  , 4  , 5  },
+                        { 6  , 7  , 8  , 9  , 10 },
+                        { 11 , 12 , 13 , 14 , 15 },
+                        { 16 , 17 , 18 , 19 , 20 },
+                        { 21 , 22 , 23 , 24 , 25 }};
+
+            printArray(m);
+
+            Console.WriteLine("Clockwise: ");
+            printArray(rotate(m, true));
+            Console.WriteLine("Counterclockwise: ");
+            printArray(rotate(m, false));
+        }
+
+        public static int[,] rotate(int[,] matrix, bool clockwise)
+        {
+            int[,] rot = new int[matrix.GetLength(0), matrix.GetLength(1)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (clockwise)
+                    {
+                        rot[i, matrix.GetLength(1) - 1 - j] = matrix[j, i];
+                    }
+                    else
+                    {
+                        rot[matrix.GetLength(0) - 1 - i, j] = matrix[j, i];
+                    }
+                }
+            }
+
+            return rot;
+        }
+
+        public static void printArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(string.Format("{0} ", array[i, j]));
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ Vigenère cipher/Vigenère cipher/VigenereCipher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VigenereCipher
+{
+    public class VigenereCipher
+    {
+        public static void Main(string[] args)
+        {
+            string key = "REDDIT";
+            string message = "TODAYISMYBIRTHDAY";
+            string ciph = VigenereCipher.crypt(message, key, true);
+            Console.WriteLine(ciph);
+            Console.WriteLine(VigenereCipher.crypt(ciph, key, false));
+        }
+
+        public static string crypt(string message, string key, bool encrypt)
+        {
+            message = message.ToUpper();
+            key = key.ToUpper();
+            string cipher = "";
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if(encrypt)
+                    cipher += (char)(((message[i] - 65) + (key[i % key.Length] - 65)) % 26 + 65);
+                else
+                    cipher += (char)(((message[i] - 65) - (key[i % key.Length] - 65) + 26) % 26 + 65);
+            }
+
+            return cipher;
+        }
+    }
+}
