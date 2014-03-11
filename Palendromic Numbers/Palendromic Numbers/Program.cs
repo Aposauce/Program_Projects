27155672c1df00c2060ad835e954c99bfa680c56
Aposauce <enclame@gmail.com>
2014-03-11 17:27:09 -0400

    added Palindromic numbers

---
+++ Palendromic Numbers/Palendromic Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromicNumbers
+{
+    public class PalindromicNumbers
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Palindromic number test:");
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine(isPalendromic(Convert.ToInt32(Console.ReadLine())));
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("Closing.");
+                Console.ReadKey();
+            }
+
+        }
+
+        public static bool isPalendromic(int num)
+        {
+            int n = num;
+            int reverse = 0;
+            while(n != 0)
+            {
+                reverse = reverse * 10;
+                reverse = reverse + n % 10;
+                n = n / 10;
+            }
+
+            return num == reverse;
+        }
+    }
+}
